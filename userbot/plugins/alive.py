16ab55cf2d6fba09c86f8b9a2e82ae14277eb294
alanwalker005xxx <66345203+alanwalker005xxx@users.noreply.github.com>
2020-06-03 08:25:52 +0530

    alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -13,8 +13,8 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars 
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
     """ For .alive command, check if the bot is running.  """
-    await alive.edit("`Jinda Hu Sarr ^.^ \nYour bot is running\n\nTelethon version: 6.9.0\nPython: 3.7.3\n\n`"
-                     f"`My peru owner`: {DEFAULTUSER}\n"
-                     "`Telethon version: 6.9.0\nPython: 3.7.3\nfork by:` @Halto_Tha\n"
-                     "`Database Status: Databases functioning normally!\n\nAlways with you, my master!\n`"
-                     "[Deploy this userbot Now](https://github.com/japarmar/GujjuBot)")
+    await alive.edit("`Yup Sir! I'm Alive ^.^ \nYour Bot Is Running\n\nTelethon Version: 6.9.0\nPython: 3.7.3\n\n`"
+                     f"`My Owner`: {DEFAULTUSER}\n"
+                     "`Telethon Version: 6.9.0\nPython: 3.7.3\nBot Made By:` @Unknown_Hacker_X\n"
+                     "`Database Status: Databases Functioning Normally!\n\nAlways With You, My Master!\n`"
+                     "[Ask My Owner To Deploy This UserBot!](https://t.me/Unknown_Hacker_X)")
